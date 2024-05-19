03fd1d176ca0a25a08ecf1954fd4f9caf223a351
eugenio-turcott <A01198189@tec.mx>
2024-05-18 23:30:03 -0600

    Primera implementación de las publicaciones

--- src/Videowall.jsx
+++ src/Videowall.jsx
@@ -5,10 +5,25 @@ import { FreeMode, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/autoplay";
+import fotochat from "./chatyipiti.png";
+import apol from "./apol.png";
+import meta from "./meta.png";
+
+const imageUrls = [
+  {
+    src: fotochat,
+    title: "Chatyipiti",
+    description: "Descripción de Chatyipiti",
+  },
+  { src: apol, title: "Apol", description: "Descripción de Apol" },
+  { src: meta, title: "Meta", description: "Descripción de Meta" },
+];
 
 const VideoWall = () => {
   const [reservaciones, setReservaciones] = useState([]);
   const [fechaActual, setFechaActual] = useState("");
+  const [flippedCards, setFlippedCards] = useState({});
+  const [shuffledImages, setShuffledImages] = useState([]);
 
   useEffect(() => {
     const fetchReservaciones = async () => {
@@ -68,6 +83,12 @@ const VideoWall = () => {
     actualizarFecha();
     const interval = setInterval(actualizarFecha, 1000); // Actualiza cada minuto
 
+    setShuffledImages(
+      [...Array(15)].map(
+        () => imageUrls[Math.floor(Math.random() * imageUrls.length)]
+      )
+    );
+
     return () => clearInterval(interval);
   }, []);
 
@@ -76,6 +97,58 @@ const VideoWall = () => {
     const ampm = hour >= 12 ? "PM" : "AM";
     const hour12 = hour % 12 || 12;
     return `${hour12}:${minute} ${ampm}`;
+  };
+
+  const handleFlip = (index) => {
+    setFlippedCards((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
+
+  const renderSwiper = (colIndex) => {
+    const isEven = colIndex % 2 === 0;
+    const direction = "vertical";
+
+    return (
+      <Swiper
+        modules={[FreeMode, Autoplay]}
+        direction={direction}
+        slidesPerView={4}
+        spaceBetween={10}
+        loop={true}
+        freeMode={true}
+        speed={5000}
+        autoplay={{
+          delay: 2,
+          disableOnInteraction: false,
+          reverseDirection: isEven,
+        }}
+        className="mySwiper"
+        style={{ height: "75vh" }}
+      >
+        {shuffledImages.map((imageUrl, index) => (
+          <SwiperSlide key={index} className="swiper-slide-custom">
+            <div
+              className={`card ${
+                flippedCards[index] ? "flipped" : ""
+              } overflow-hidden`}
+              onClick={() => handleFlip(index)}
+            >
+              <div className="card-inner">
+                <div className="card-front">
+                  <img src={imageUrl.src} alt={imageUrl.title} />
+                </div>
+                <div className="card-back">
+                  <h2>{imageUrl.title}</h2>
+                  <p>{imageUrl.description}</p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    );
   };
 
   return (
@@ -134,15 +207,12 @@ const VideoWall = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-8 flex-1 bg-gray-300">
-        <div className="col-span-1 bg-gray-400">Columna 1</div>
-        <div className="col-span-1 bg-gray-500">Columna 2</div>
-        <div className="col-span-1 bg-gray-400">Columna 3</div>
-        <div className="col-span-1 bg-gray-500">Columna 4</div>
-        <div className="col-span-1 bg-gray-400">Columna 5</div>
-        <div className="col-span-1 bg-gray-500">Columna 6</div>
-        <div className="col-span-1 bg-gray-400">Columna 7</div>
-        <div className="col-span-1 bg-gray-500">Columna 8</div>
+      <div className="grid grid-cols-8 flex-1 background-gradient">
+        {Array.from({ length: 7 }, (_, colIndex) => (
+          <div className="col-span-1 overflow-hidden" key={colIndex}>
+            {renderSwiper(colIndex + 1)}
+          </div>
+        ))}
       </div>
     </div>
   );
